ae06cc5cc3a921188ce1fc2c1d3b93f88cc8d371
Tobe12345678 <ogunedotobechukwu@gmail.com>
2026-02-20 15:16:53 +0000

    Removed old files

--- interview_practice2.py
+++
@@ -1,5 +0,0 @@
-routers = ['router1', 'router2', 'router3']
-
-
-routers.append('router4')
-print(routers)
--- interview_practice3.py
+++
@@ -1,9 +0,0 @@
-import requests
-from request.auth import HTTPBasicAuth
-
-router_ip = "10.10.20.48"
-username = "Router2"
-password = "cisco123"
-
-https_url = f"https://{router_ip}"
-
